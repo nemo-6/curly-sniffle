2225e6149026bc5a1acad6ea06b085c6756cb622
Nemo Alex <a130120642@gmail.com>
2019-04-04 20:45:28 -0300

    add puns endpoint

--- main.go
+++ main.go
@@ -3,14 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/potato", func(w http.ResponseWriter, r *http.Request) {
+
+	// Test endpoint
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Method: "+r.Method+"\n")
 		fmt.Fprintf(w, "URL: "+r.URL.Host+"\n")
 		fmt.Fprintf(w, "Host: "+r.Host+"\n")
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Puns endpoint.
+	http.HandleFunc("/pun", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().Unix())
+		jokes := []string{
+			"Q: Why did the scarecrow win an award?\nA: Because he was outstanding in his field",
+			"Q: What did the inspector molecule say to the suspect molecule?\nA: \"I've got my ion you\"",
+			"Q: Did you hear about the cheese factory that exploded in France?\nA: There was nothing left but de brie",
+			"I just watched a documentary on beavers. It was the best dam program I've ever seen.",
+			"A prisoner asked to take his own mugshot. They called it a cellfie.",
+			"I'm not addicted to brake fluid, I can stop whenever I want.",
+			"Q: What do you call cheese by itself?\nA: Provolone",
+			"Q: Why do crabs never give to charity?\nA: Because they're shellfish",
+			"Don't trust atoms. They make up everything.",
+			"Q: Why can't you hear a pterodactyl go to the bathroom?\nA: Because the P is silent.",
+		}
+		n := rand.Int() % len(jokes)
+		fmt.Fprintf(w, jokes[n])
+	})
+
+	// Start serving
+	log.Fatal(http.ListenAndServe(":2983", nil))
 }
